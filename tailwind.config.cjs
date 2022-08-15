093b2c3e2b0e54535b3e2e5c2b06905c9a6958e3
Javier Reyes <javier.reys@gmail.com>
2022-08-15 03:57:59 -0400

    chore(deps): add tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.html",
+    "./src/**/*.{astro,js,jsx,svelte,ts,tsx,vue,md}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
