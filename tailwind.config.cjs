b97d60d1d7e01609b9780902ff184de874b00772
Javier Reyes <javier.reys@gmail.com>
2022-09-10 03:30:22 -0300

    build: add mdx to tailwind content

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,7 +8,7 @@ const {
 module.exports = {
   content: [
     "./public/**/*.html",
-    "./src/**/*.{astro,js,jsx,svelte,ts,tsx,vue,md}",
+    "./src/**/*.{astro,js,jsx,svelte,ts,tsx,vue,md,mdx}",
   ],
   darkMode: "class",
   theme: {
