80c7957b94d8fbdbe0c26164553ebc123f252dc3
Javier Reyes <javier.reys@gmail.com>
2022-09-10 01:16:29 -0300

    feat: fetch articles from articles folder

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  integrations: [react()],
+  integrations: [react(), mdx()],
 });
