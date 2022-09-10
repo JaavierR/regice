2b859d11b9404eff5cfa6d1d2e608da90a64add0
Javier Reyes <javier.reys@gmail.com>
2022-09-10 03:18:44 -0300

    refactor: change color theme to pink

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -33,17 +33,17 @@ function MoonIcon({
         fillRule="evenodd"
         clipRule="evenodd"
         d="M17.715 15.15A6.5 6.5 0 0 1 9 6.035C6.106 6.922 4 9.645 4 12.867c0 3.94 3.153 7.136 7.042 7.136 3.101 0 5.734-2.032 6.673-4.853Z"
-        className={selected ? "fill-cyan-400/20" : "fill-transparent"}
+        className={selected ? "fill-pink-400/20" : "fill-transparent"}
       />
       <path
         d="m17.715 15.15.95.316a1 1 0 0 0-1.445-1.185l.495.869ZM9 6.035l.846.534a1 1 0 0 0-1.14-1.49L9 6.035Zm8.221 8.246a5.47 5.47 0 0 1-2.72.718v2a7.47 7.47 0 0 0 3.71-.98l-.99-1.738Zm-2.72.718A5.5 5.5 0 0 1 9 9.5H7a7.5 7.5 0 0 0 7.5 7.5v-2ZM9 9.5c0-1.079.31-2.082.845-2.93L8.153 5.5A7.47 7.47 0 0 0 7 9.5h2Zm-4 3.368C5 10.089 6.815 7.75 9.292 6.99L8.706 5.08C5.397 6.094 3 9.201 3 12.867h2Zm6.042 6.136C7.718 19.003 5 16.268 5 12.867H3c0 4.48 3.588 8.136 8.042 8.136v-2Zm5.725-4.17c-.81 2.433-3.074 4.17-5.725 4.17v2c3.552 0 6.553-2.327 7.622-5.537l-1.897-.632Z"
-        className={selected ? "fill-cyan-500" : "fill-slate-400"}
+        className={selected ? "fill-pink-500" : "fill-zinc-400"}
       />
       <path
         fillRule="evenodd"
         clipRule="evenodd"
         d="M17 3a1 1 0 0 1 1 1 2 2 0 0 0 2 2 1 1 0 1 1 0 2 2 2 0 0 0-2 2 1 1 0 1 1-2 0 2 2 0 0 0-2-2 1 1 0 1 1 0-2 2 2 0 0 0 2-2 1 1 0 0 1 1-1Z"
-        className={selected ? "fill-cyan-500" : "fill-slate-400"}
+        className={selected ? "fill-pink-500" : "fill-zinc-400"}
       />
     </svg>
   );
@@ -63,7 +63,7 @@ function PcIcon({
         strokeWidth="2"
         strokeLinejoin="round"
         className={
-          selected ? "fill-cyan-400/20 stroke-cyan-500" : "stroke-slate-400"
+          selected ? "fill-pink-400/20 stroke-pink-500" : "stroke-zinc-400"
         }
       />
       <path
@@ -71,7 +71,7 @@ function PcIcon({
         strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
-        className={selected ? "stroke-cyan-500" : "stroke-slate-400"}
+        className={selected ? "stroke-pink-500" : "stroke-zinc-400"}
       />
     </svg>
   );
@@ -96,12 +96,12 @@ function SunIcon({
       <path
         d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
         className={
-          selected ? "fill-cyan-400/20 stroke-cyan-500" : "stroke-slate-400"
+          selected ? "fill-pink-400/20 stroke-pink-500" : "stroke-zinc-400"
         }
       />
       <path
         d="M12 4v1M17.66 6.344l-.828.828M20.005 12.004h-1M17.66 17.664l-.828-.828M12 20.01V19M6.34 17.664l.835-.836M3.995 12.004h1.01M6 6l.835.836"
-        className={selected ? "stroke-cyan-500" : "stroke-slate-400"}
+        className={selected ? "stroke-pink-500" : "stroke-zinc-400"}
       />
     </svg>
   );
@@ -127,7 +127,7 @@ export function ThemeToggle({ panelClassName = "mt-4" }) {
       <Listbox.Label className="sr-only">Theme</Listbox.Label>
       <Listbox.Button
         type="button"
-        className="flex h-6 w-6 items-center justify-center rounded-lg shadow-md shadow-black/5 ring-1 ring-black/5 dark:bg-slate-700 dark:ring-inset dark:ring-white/5"
+        className="flex h-6 w-6 items-center justify-center rounded-lg shadow-md shadow-black/5 ring-1 ring-black/5 dark:bg-zinc-700 dark:ring-inset dark:ring-white/5"
       >
         <span className="dark:hidden">
           <SunIcon className="h-5 w-5" selected={selectedTheme !== "system"} />
@@ -138,7 +138,7 @@ export function ThemeToggle({ panelClassName = "mt-4" }) {
       </Listbox.Button>
       <Listbox.Options
         className={clsx(
-          "absolute top-full right-0 z-50 w-36 overflow-hidden rounded-lg bg-white py-1 text-sm font-semibold text-slate-700 shadow-lg ring-1 ring-slate-900/10 dark:bg-slate-800 dark:text-slate-300 dark:ring-0 dark:highlight-white/5",
+          "absolute top-full right-0 z-50 w-36 overflow-hidden rounded-lg bg-white py-1 text-sm font-semibold text-zinc-700 shadow-lg ring-1 ring-zinc-900/10 dark:bg-zinc-800 dark:text-zinc-300 dark:ring-0 dark:highlight-white/5",
           panelClassName
         )}
       >
@@ -148,8 +148,8 @@ export function ThemeToggle({ panelClassName = "mt-4" }) {
               <li
                 className={clsx(
                   "flex cursor-pointer items-center py-1 px-2",
-                  selected && "text-cyan-500",
-                  active && "bg-slate-50 dark:bg-slate-600/30"
+                  selected && "text-pink-500",
+                  active && "bg-zinc-50 dark:bg-zinc-600/30"
                 )}
               >
                 <Icon selected={selected} className="mr-4 h-6 w-6" />
