c8fd6b61530b28444e4a11ffb4a0920282c22390
Javier Reyes <javier.reys@gmail.com>
2022-09-11 06:10:16 -0300

    feat: add tw typo plugin with mdx config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,21 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
 import mdx from "@astrojs/mdx";
+import remarkGfm from "remark-gfm";
+import rehypePrism from "@mapbox/rehype-prism";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  integrations: [react(), mdx()],
+  markdown: {
+    syntaxHighlight: "prism",
+  },
+  integrations: [
+    react(),
+    mdx({
+      remarkPlugins: [remarkGfm],
+      rehypePlugins: [rehypePrism],
+    }),
+  ],
 });
