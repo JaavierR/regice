ed1585a0d15dcea0ff0a3cf8b7ec1f354e3c717c
Javier Reyes <javier.reys@gmail.com>
2022-09-21 13:59:33 -0300

    refactor: change look and feel (#2)
    * chore(deps): bump all deps
    
    * feat: fetch articles from articles folder
    
    * refactor: update title text
    
    * refactor: change color theme to pink
    
    * refactor: change hero display
    
    * feat: change navbar and add link to important sections
    
    * feat: add tailwind to all pages
    
    * build: add mdx to tailwind content
    
    * feat: add layout to mdx file
    
    * feat: add profile picture to navbar on all pages except home
    
    * style: change font weight on hero
    
    * feat: add profile picture to index page
    
    * fix: change year of first post
    
    * feat: add footer
    
    * feat: add container components
    
    * feat: add global layout
    
    * refactor: pass class attributes to astro components (container)
    
    * refactor: apply containers to simplify the layout
    
    * fix: check if dateTime is available to apply fn
    
    * refactor: remove unused code
    
    * refactor: remove unused components
    
    * build: update tailwind config
    
    * feat: add card component
    
    * feat: add projects page
    
    * refactor: change navbar
    
    * refactor: inline header script
    
    * feat: add desktop navigation
    
    * feat: set bg colors to theme toggle
    
    * fix: add fragment to avoid export error
    
    * refactor: remove avatar scroll effect
    
    * refactor: remove unused component
    
    * feat: add image to hero and remove home icon from home page
    
    * feat: add stack card to index page
    
    * feat: add about page
    
    * feat: add project logos and tech used
    
    * feat: add nextjs logo to projects
    
    * refactor: resize theme toggle
    
    * feat: add target blank to projects link
    
    * feat: add tw typo plugin with mdx config
    
    * fix: set global path to profile img
    
    * chore(config): associate mdx to markdown at vscode level
    
    * refactor: remove padding from project card
    
    * feat: add article layout
    
    * feat: add 404 page
    
    * refactor: remove download cv button
    
    * feat: update main description
    
    * chore: add astrojs/image package
    
    * feat: use astro image instead of img
    
    * feat: enable scroll effect on home page
    
    * fix: put header on top of the content
    
    * feat: add mobile navigation
    
    * fix: change contrast of some texts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,24 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
+import mdx from "@astrojs/mdx";
+import remarkGfm from "remark-gfm";
+import rehypePrism from "@mapbox/rehype-prism";
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  integrations: [react()],
+  markdown: {
+    syntaxHighlight: "prism",
+  },
+  integrations: [
+    react(),
+    image(),
+    mdx({
+      remarkPlugins: [remarkGfm],
+      rehypePlugins: [rehypePrism],
+    }),
+  ],
 });
---
+++ src/components/MobileNavigation.tsx
@@ -0,0 +1,102 @@
+import { Fragment } from "react";
+import { Popover, Transition } from "@headlessui/react";
+import type { HTMLAttributes, ReactNode } from "react";
+
+function CloseIcon(props: HTMLAttributes<SVGElement>) {
+  return (
+    <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
+      <path
+        d="m17.25 6.75-10.5 10.5M6.75 6.75l10.5 10.5"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+function ChevronDownIcon(props: HTMLAttributes<SVGElement>) {
+  return (
+    <svg viewBox="0 0 8 6" aria-hidden="true" {...props}>
+      <path
+        d="M1.75 1.75 4 4.25l2.25-2.5"
+        fill="none"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+function MobileNavItem({
+  href,
+  children,
+}: {
+  href: string;
+  children: ReactNode;
+}) {
+  return (
+    <li>
+      <Popover.Button as="a" href={href} className="block py-2">
+        {children}
+      </Popover.Button>
+    </li>
+  );
+}
+
+export default function MobileNavigation(props) {
+  return (
+    <Popover {...props}>
+      <Popover.Button className="group flex items-center rounded-full bg-white/90 px-4 py-2 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10 dark:hover:ring-white/20">
+        Menu
+        <ChevronDownIcon className="ml-3 h-auto w-2 stroke-zinc-500 group-hover:stroke-zinc-700 dark:group-hover:stroke-zinc-400" />
+      </Popover.Button>
+      <Transition.Root>
+        <Transition.Child
+          as={Fragment}
+          enter="duration-150 ease-out"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="duration-150 ease-in"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <Popover.Overlay className="fixed inset-0 z-50 bg-zinc-800/40 backdrop-blur-sm dark:bg-black/80" />
+        </Transition.Child>
+        <Transition.Child
+          as={Fragment}
+          enter="duration-150 ease-out"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="duration-150 ease-in"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+        >
+          <Popover.Panel
+            focus
+            className="fixed inset-x-4 top-8 z-50 origin-top rounded-3xl bg-white p-8 ring-1 ring-zinc-900/5 dark:bg-zinc-900 dark:ring-zinc-800"
+          >
+            <div className="flex flex-row-reverse items-center justify-between">
+              <Popover.Button aria-label="Close menu" className="-m-1 p-1">
+                <CloseIcon className="h-6 w-6 text-zinc-500 dark:text-zinc-400" />
+              </Popover.Button>
+              <h2 className="text-sm font-medium text-zinc-600 dark:text-zinc-400">
+                Navigation
+              </h2>
+            </div>
+            <nav className="mt-6">
+              <ul className="-my-2 divide-y divide-zinc-100 text-base text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
+                <MobileNavItem href="/about">About</MobileNavItem>
+                <MobileNavItem href="/articles">Articles</MobileNavItem>
+                <MobileNavItem href="/projects">Projects</MobileNavItem>
+              </ul>
+            </nav>
+          </Popover.Panel>
+        </Transition.Child>
+      </Transition.Root>
+    </Popover>
+  );
+}
