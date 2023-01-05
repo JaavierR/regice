58fcdc534e27cf04529dd1a3c3974fb3b44b115a
Javier <javier.reys@gmail.com>
2023-01-05 03:47:03 -0300

    feat: add new color palettes (#3)
    * chore: add magenta shades
    
    * refactor: change hero colors to magenta and use rsquo
    
    * refactor: update article link color to magenta

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -348,6 +348,21 @@ module.exports = {
           to: { backgroundPosition: "200% center" },
         },
       },
+      colors: {
+        magenta: {
+          DEFAULT: "#bb2649",
+          50: "#fef2f3",
+          100: "#fce7e8",
+          200: "#f9d2d7",
+          300: "#f4adb5",
+          400: "#ed7f8e",
+          500: "#e25169",
+          600: "#cd3152",
+          700: "#bb2649",
+          800: "#91203e",
+          900: "#7c1f3a",
+        },
+      },
     },
   },
 };
